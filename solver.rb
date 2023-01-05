a58843d70e710c18bd75710d33889695cf5fecfe
joker69216 <hamaraad883@gmail.com>
2023-01-05 19:06:43 +0300

    fix minor bug in the solver class / add the needed tests

--- solver.rb
+++ solver.rb
@@ -1,5 +1,7 @@
 class Solver
   def self.factorial(n)
+    return 'Error' if n.class == String
+    return 'Error' if n < 0
     return 1 if n <= 1
     factorial(n-1) * n
   end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,6 +11,18 @@ describe Solver do
       expect(Solver.factorial(0)).to eql(1)
     end
 
+    it 'should return 1 to factorial of 1.' do
+      expect(Solver.factorial(1)).to eql(1)
+    end
+
+    it 'should send backa  string saying error.' do
+      expect(Solver.factorial(-1)).to eql('Error')
+    end
+
+    it 'should send back an error if the the parameter is a String' do
+      expect(Solver.factorial('a')).to eql('Error')
+    end
+
     it 'should return 6 to factorial of 3.' do
       expect(Solver.factorial(3)).to eql(6)
     end
